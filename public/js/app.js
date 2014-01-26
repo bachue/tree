ba8591c9c4daa85a7fc196f96a7de0fa08309bd1
Bachue Zhou <bachue.shu@gmail.com>
2014-01-26 22:44:28 +0800

    add lots of js files and ready for angular!

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+define(['angular', 'controllers', 'directives', 'filters', 'angular_ui_router'], function(angular) {
+    angular.module('TreeApp', ['controllers', 'directives', 'filters', 'ui.router']);
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,35 @@
+var shim = {
+    underscope: {
+        exports: '_'
+    },
+    angular: {
+        exports: 'angular',
+        deps: ['jquery']
+    },
+    angular_ui_router {
+        deps: ['angular']
+    },
+    bootstrap: {
+        deps: ['jquery']
+    }
+}
 
+require.config({
+    paths: {
+        domReady: 'vendor/js/domReady',
+        jquery: 'vendor/js/jquery',
+        underscope: 'vendor/js/underscope',
+        angular: 'vendor/js/angular',
+        angular_ui_router: 'vendor/js/angular-ui-router.min',
+        bootstrap: 'vendor/js/bootstrap',
+    },
+    shim: shim
+});
+
+require(['angular', 'app', 'domReady', 'jquery'], function(angular, app, domReady, $) {
+    app.config(function() {
+        domReady(function() {
+            angular.bootstrap(document, ['TreeApp']);
+        });
+    });
+});
