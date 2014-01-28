27a5c421337960082137a2e961cf0a88eb7b2c58
Bachue Zhou <bachue.shu@gmail.com>
2014-01-28 15:06:01 +0800

    now `git clone` from config dialog can work well

--- public/js/controllers/application.js
+++ public/js/controllers/application.js
@@ -1,6 +1,23 @@
 define(['controllers', 'promise!loaders/projects'], function(controllers, projects) {
     return controllers.controller('Application', function($scope, $state, $timeout, Restangular) {
         $scope.current = {};
+        $scope.current.config_dialog = {branch: 'master'};
+
+        $scope.submit_config = function() {
+            var loader = Restangular.all('projects');
+            $scope.current.config_dialog.cloning = true;
+            loader.post($scope.current.config_dialog).then(function(result) {
+                if (result == 'true') {
+                    loader.getList().then(function(projects) {
+                        $timeout(function() {
+                            $scope.projects = projects;
+                            $scope.current.config_dialog = {branch: 'master'};
+                            $('#project-config').modal('hide');
+                        });
+                    });
+                };
+            });
+        };
 
         $scope.projects = projects;
         $state.go('application.project');
