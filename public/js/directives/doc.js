e54c856d6f7acb90a36028efc886e5aa351c7dcf
Bachue Zhou <bachue.shu@gmail.com>
2014-01-29 20:13:32 +0800

    Now sections finished

--- public/js/directives/doc.js
+++ public/js/directives/doc.js
@@ -2,11 +2,15 @@ define(['directives'], function(directives) {
     directives.directive('autoAnchors', function($timeout, $state, $location, $anchorScroll) {
         return {
             link: function(scope, element, attrs) {
-                $timeout(function() { do_with(element); });
-                function do_with(element) {
+                $timeout(function() { do_with(scope, element); }, 100);
+                function do_with(scope, element) {
+                    scope.current.sections = [];
+                    var feed = 0;
                     $(element).find('h1,h2,h3,h4,h5,h6').each(function(i, e) {
-                        var id = e.innerText.replace(/\s/g, '-').replace(/[^\w-]/, '').toLowerCase();
+                        var id = e.innerText.replace(/\s/g, '-').replace(/[^\w-]/g, '').toLowerCase();
+                        if(!id) id = ++feed;
                         var anchor = $('<a class="anchor" id="section-'+id+'"></a>');
+                        scope.current.sections.push({level: e.nodeName[1], text: e.innerText, id: id});
                         anchor.prependTo(e);
                     });
                 }
